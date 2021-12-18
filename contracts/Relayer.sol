92bee4b089a6ed24b6df92ea968c41c6636a4469
lukepark327 <lukepark327@gmail.com>
2021-12-18 23:46:24 +0900

    change contract structure from Dispatcher.sol to Relayer.sol: for DAO

--- contracts/Relayer.sol
+++ contracts/Relayer.sol
@@ -5,15 +5,15 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
- * @title Dispatcher
+ * @title Relayer
  * 
- * Inspired by multicall, timelock and permit contract.
+ * Inspired by multicall, timelock contracts and permit.
  *
  * - https://github.com/makerdao/multicall/blob/master/src/Multicall.sol
  * - https://github.com/compound-finance/compound-protocol/blob/v2.8.1/contracts/Timelock.sol
  * - https://github.com/Uniswap/v2-core/blob/v1.0.1/contracts/UniswapV2ERC20.sol
  */
-contract Dispatcher is Ownable {
+contract Relayer is Ownable {
     // TODO: gas limit per Call.
 
     struct Call {
@@ -23,17 +23,34 @@ contract Dispatcher is Ownable {
         string signature; // function selector
         bytes data; // args
     }
-
     struct Sig {
         uint8 v; bytes32 r; bytes32 s;
     }
-
     struct Result {
         bool success;
         bytes returnData;
         uint256 returnWei;
     }
 
+    // ETH Vault
+    // TODO: ERC20, et al. from DAO governance.
+    mapping (address => uint256) public vault;
+
+    function deposit() public payable {
+        vault[msg.sender] += msg.value;
+    }
+
+    function withdraw(uint256 amount) public returns (Result memory returnData) {
+        // TODO: check overflow test logic contains this line.
+        require(vault[msg.sender] >= amount, "Relayer: INVALID_AMOUNT");
+        vault[msg.sender] -= amount;
+        (bool success, bytes memory ret) = msg.sender.call{value: amount}("");
+        returnData = Result(success, ret, amount);
+    }
+
+    /**
+     * @dev Implement IBATCH and ETH receiving logic.
+     */
     function tryDispatch(
         Call[] memory calls, // be a bmsg
         Sig[] memory sigs
@@ -60,27 +77,17 @@ contract Dispatcher is Ownable {
             address recoveredAddress = ecrecover(digest, sigEach.v, sigEach.r, sigEach.s);
             require(
                 recoveredAddress != address(0) && recoveredAddress == callEach.account,
-                "INVALID_SIGNATURE"
+                "Relayer: INVALID_SIGNATURE"
             );
 
-            bytes memory callData;
-            if (bytes(callEach.signature).length == 0) {
-                callData = callEach.data;
-            } else {
-                callData = abi.encodePacked(
-                    bytes4(keccak256(bytes(callEach.signature))), callEach.data
-                );
-            }
-
-            (bool success, bytes memory ret) = callEach.target.call{value: callEach.value}(callData);            
-            returnData[i] = Result(success, ret, address(this).balance);
-
-            if (address(this).balance != 0) {
-                callEach.account.call{value: address(this).balance}("");
-            }
+            returnData[i] = _call(callEach);
         }   
     }
 
+    /**
+     * @dev Single transaction call.
+     * Using for RTC (Remote Transaction Call) relaying.
+     */
     function trySingleCall(
         Call memory singleCall, // be a bmsg
         Sig memory singleSig
@@ -101,8 +108,18 @@ contract Dispatcher is Ownable {
         address recoveredAddress = ecrecover(digest, singleSig.v, singleSig.r, singleSig.s);
         require(
             recoveredAddress != address(0) && recoveredAddress == singleCall.account,
-            "INVALID_SIGNATURE"
+            "Relayer: INVALID_SIGNATURE"
         );
+
+        returnData = _call(singleCall);
+    }
+
+    function _call(
+        Call memory singleCall
+    ) internal returns (
+        Result memory returnData
+    ) {
+        uint256 prevBalance = address(this).balance;
 
         bytes memory callData;
         if (bytes(singleCall.signature).length == 0) {
@@ -113,16 +130,18 @@ contract Dispatcher is Ownable {
             );
         }
 
-        (bool success, bytes memory ret) = singleCall.target.call{value: singleCall.value}(callData);            
-        returnData = Result(success, ret, address(this).balance);
-
-        if (address(this).balance != 0) {
-            singleCall.account.call{value: address(this).balance}("");
+        if (singleCall.value != 0) {
+            // TODO: check overflow test logic contains this line.
+            require(vault[singleCall.account] >= singleCall.value, "Relayer: INVALID_AMOUNT");
+            vault[singleCall.account] -= singleCall.value;
         }
-    }
+        (bool success, bytes memory ret) = singleCall.target.call{value: singleCall.value}(callData);
 
-    function withdrawEth() public onlyOwner {
-        msg.sender.call{value: address(this).balance}("");
+        uint256 receivedETH = address(this).balance - prevBalance;
+        returnData = Result(success, ret, receivedETH);
+        if (receivedETH != 0) {
+            vault[singleCall.account] += receivedETH;
+        }
     }
 
     receive() external payable { }
