f1d906790a4e03d269d87e3288b63e17bea9833d
lukepark327 <lukepark327@gmail.com>
2021-12-18 23:05:13 +0900

    Fix title typo

--- contracts/Dispatcher.sol
+++ contracts/Dispatcher.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
- * @title Relayer
+ * @title Dispatcher
  * 
  * Inspired by multicall, timelock and permit contract.
  *
